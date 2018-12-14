b9c93a6e8013336a9df0619a7755e5c7cb6040d2
Tsuyoshi Ichikawa <ichikawa.develop@tulipsoftware.work>
2018-12-14 21:42:27 +0900

    Fix proxy setting position
    Move before vagrant plugin install.

--- ubuntu-18.04-gitlab-ce/setup-vagrant.bat
+++ ubuntu-18.04-gitlab-ce/setup-vagrant.bat
@@ -1,10 +1,10 @@
 @echo off
-: Install plugins
-vagrant plugin install vagrant-proxyconf
-vagrant plugin install vagrant-teraterm
-
 : In the proxy environment, please set the following
 : set http_proxy=http://
 : set https_proxy=http://
+
+: Install plugins
+vagrant plugin install vagrant-proxyconf
+vagrant plugin install vagrant-teraterm
 
 vagrant up --provision
